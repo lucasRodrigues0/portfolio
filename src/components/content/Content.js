77c5b5f6db0b2c7abfc2162b6fe2b68085b2d1e2
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-02-19 17:48:04 -0300

    estilização

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -2,37 +2,66 @@
 import { SKILLS, PROJECTS } from '../../utils/constants';
 import styled from "styled-components";
 
-const ContentContainer = styled.section`
+const ContentContainer = styled.div`
   background-color: #010005;
   color: white;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 90%;
+  border: 1px solid red;
 `
 
 const SectionContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  border: 1px solid green;
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+`
+
+const Title = styled.h1`
+  background-color: transparent;
+  font-size: 70px;
+  width: fit-content;
+  transition: text-shadow 0.3s ease-out;
+  &: hover {
+    transition: text-shadow 0.3s ease-in;
+    text-shadow: 5px 3px 22px rgba(66,1,65,0.79);
+  }
+`;
+
+const Paragraph = styled.p`
+  width: 50%;
+  font-size: 30px;
+  margin-left: 25%;
 `
 
 export const Content = () => {
   return (
     <ContentContainer>
       <SectionContainer>
-        <section>
-          <h1>About Me</h1>
-          <p>Hello, I'm Lucas, a Brazilian web developer. I hold a degree in Systems Development and Analysis. With four years of experience in IT, specializing in change management, I'm now eager to transition into a role as a Junior Front-End Developer.</p>
-        </section>
-      </SectionContainer>
-      <section>
-          <h1>Skills</h1>
+        <Section>
+          <Title>About Me</Title>
+          <Paragraph>Hello, I'm Lucas, a Brazilian web developer. I hold a degree in Systems Development and Analysis. With four years of experience in IT, specializing in change management, I'm now eager to transition into a role as a Junior Front-End Developer.</Paragraph>
+        </Section>
+        <Section>
+          <Title dir="right">Skills</Title>
           {
             SKILLS.map((skill) => {
               return (
                 <div>
                   <h5>{skill}</h5>
                 </div>
-              )
+              );
             })
           }
-      </section>
-      <section>
+        </Section>
+      </SectionContainer>
+      {/* <section>
         <h1>Personal Projects</h1>
         {
           PROJECTS.map((proj) => {
@@ -47,7 +76,7 @@ export const Content = () => {
             )
           })
         }
-      </section>
+      </section> */}
     </ContentContainer>
   )
 }
